3bfce28d7e2ee84f775b2fe16b1c18de34c8b039
Mayankdhote18 <mayankdhote4316@gmail.com>
2025-08-12 20:25:30 +0530

    implemented real time databse in the project to save data Used firebase as a dataabse

--- app/src/main/java/com/example/splashscreen/SignUp.kt
+++ app/src/main/java/com/example/splashscreen/SignUp.kt
@@ -1,12 +1,20 @@
 package com.example.splashscreen
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +25,23 @@ class SignUp : AppCompatActivity() {
             insets
         }
 
+        val signupButton = findViewById<Button>(R.id.button4)  // <-- fixed parentheses
+        val etName = findViewById<TextInputEditText>(R.id.etName)
+        val etMail = findViewById<TextInputEditText>(R.id.etEmail)
+        val etPassword = findViewById<TextInputEditText>(R.id.etPassword)
 
+        signupButton.setOnClickListener {
+            val name = etName.text.toString()
+            val mail = etMail.text.toString()
+            val password = etPassword.text.toString()
+
+            val user = User(name, mail, password)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            database.child(name).setValue(user).addOnSuccessListener {
+                Toast.makeText(this, "user registered successfully", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {  // <-- changed to addOnFailureListener
+                Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
